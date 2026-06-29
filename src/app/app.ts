bd0ce655c4252de848fb080960707f93baaf0a87
JScorpioDv <juanpablosierra36@gmail.com>
2026-06-29 18:47:24 +0200

    :sparkles: Feat: Ionic Angular Ionic Angular installed in project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [IonApp, IonRouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true
 })
-export class App {
-  protected readonly title = signal('calismap-app');
-}
+export class App {}
